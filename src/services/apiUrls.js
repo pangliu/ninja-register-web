53137b3213c3f4ef112b8b789fd73ba4a5783e1f
hank Liu <hankliu@hankdeMacBook-Air.local>
2025-01-06 14:26:52 +0800

    [fix]修正baseUrl

--- src/services/apiUrls.js
+++ src/services/apiUrls.js
@@ -2,7 +2,7 @@ class ApiUrls {
     static TEST_BASE_URL = 'https://run.mocky.io/';
     static TEST_REGISTER = 'v3/b9de7428-8113-46a4-8501-f9bbf7d2a69a';
 
-    static BASE_URL = 'https://5ca6-18-178-159-1.ngrok-free.app/';
+    static BASE_URL = 'https://bf24-118-163-110-217.ngrok-free.app/';
     // register
     static REGISTER = 'api/register';
 }
